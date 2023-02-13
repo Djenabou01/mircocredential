d5595f05495bb7feb4661b98a636506f162f9f8f
Djenabou01 <121902274+Djenabou01@users.noreply.github.com>
2023-02-12 21:23:55 -0500

    Add files via upload

---
+++ project 2/project2.js
@@ -0,0 +1,175 @@
+console.log('Mist & Memories')
+let item1 = document.querySelector('.Link1')
+let item2 = document.querySelector('.Link2')
+let item3 = document.querySelector('.Link3')
+let item4 = document.querySelector('.Link4')
+let item5 = document.querySelector('.Link5')
+let item6 = document.querySelector('.Link6')
+let item7 = document.querySelector('.Link7')
+let item8 = document.querySelector('.Link8')
+//Part2
+let item9 = document.querySelector('.Link9')
+let item10 = document.querySelector('.Link10')
+let item11 = document.querySelector('.Link11')
+let item12 = document.querySelector('.Link12')
+let item13 = document.querySelector('.Link13')
+let item14 = document.querySelector('.Link14')
+let item15 = document.querySelector('.Link15')
+let item16 = document.querySelector('.Link16')
+
+//part1
+let modal =document.querySelector('.modalWindow')
+let modal1 =document.querySelector('.img2')
+let modal2 =document.querySelector('.img3')
+let modal3 =document.querySelector('.img4')
+let modal4 =document.querySelector('.img5')
+let modal5 =document.querySelector('.img6')
+let modal6 =document.querySelector('.img7')
+let modal7 =document.querySelector('.img8')
+//part2
+let modal8 =document.querySelector('.img9')
+let modal9 =document.querySelector('.img10')
+let modal10 =document.querySelector('.img11')
+let modal11 =document.querySelector('.img12')
+let modal12 =document.querySelector('.img13')
+let modal13 =document.querySelector('.img14')
+let modal14 =document.querySelector('.img15')
+let modal15 =document.querySelector('.img16')
+
+
+//part1
+let xclose = document.querySelector('.modalHeadX')
+let close2 = document.querySelector('.modalHeadX2')
+let close3 = document.querySelector('.modalHeadX3')
+let close4 = document.querySelector('.modalHeadX4')
+let close5 = document.querySelector('.modalHeadX5')
+let close6 = document.querySelector('.modalHeadX6')
+let close7 = document.querySelector('.modalHeadX7')
+let close8 = document.querySelector('.modalHeadX8')
+//part2
+let close9 = document.querySelector('.modalHeadX9')
+let close10 = document.querySelector('.modalHeadX10')
+let close11 = document.querySelector('.modalHeadX11')
+let close12 = document.querySelector('.modalHeadX12')
+let close13 = document.querySelector('.modalHeadX13')
+let close14 = document.querySelector('.modalHeadX14')
+let close16 = document.querySelector('.modalHeadX15')
+let close17 = document.querySelector('.modalHeadX16')
+
+//part1
+item1.addEventListener('click', function(){
+    modal.style.display = 'block'
+
+})
+item2.addEventListener('click', function(){
+    modal1.style.display = 'block'
+
+})
+item3.addEventListener('click', function(){
+    modal2.style.display = 'block'
+
+})
+item4.addEventListener('click', function(){
+    modal3.style.display = 'block'
+
+})
+item5.addEventListener('click', function(){
+    modal4.style.display = 'block'
+
+})
+item6.addEventListener('click', function(){
+    modal5.style.display = 'block'
+
+})
+item7.addEventListener('click', function(){
+    modal6.style.display = 'block'
+
+})
+item8.addEventListener('click', function(){
+    modal7.style.display = 'block'
+
+})
+//part 2
+item9.addEventListener('click', function(){
+    modal8.style.display = 'block'
+
+})
+item10.addEventListener('click', function(){
+    modal9.style.display = 'block'
+
+})
+item11.addEventListener('click', function(){
+    modal10.style.display = 'block'
+
+})
+item12.addEventListener('click', function(){
+    modal11.style.display = 'block'
+
+})
+item13.addEventListener('click', function(){
+    modal12.style.display = 'block'
+
+})
+item14.addEventListener('click', function(){
+    modal13.style.display = 'block'
+
+})
+item15.addEventListener('click', function(){
+    modal14.style.display = 'block'
+
+})
+item16.addEventListener('click', function(){
+    modal15.style.display = 'block'
+
+})
+//part1
+
+xclose.addEventListener('click', function(){
+    modal.style.display = 'none'
+ })
+ close2.addEventListener('click',function(){
+    modal1.style.display='none'    
+})
+close3.addEventListener('click',function(){
+    modal2.style.display='none'    
+})
+close4.addEventListener('click',function(){
+    modal3.style.display='none'    
+})
+close5.addEventListener('click',function(){
+    modal4.style.display='none'    
+ })
+close6.addEventListener('click',function(){
+    modal5.style.display='none'    
+})
+close7.addEventListener('click',function(){
+    modal6.style.display='none'    
+ })
+close8.addEventListener('click',function(){
+    modal7.style.display='none'    
+})
+// part 2 of eventlistener
+close9.addEventListener('click', function(){
+    modal8.style.display = 'none'
+ })
+close10.addEventListener('click',function(){
+    modal9.style.display='none'    
+})
+close11.addEventListener('click',function(){
+    modal10.style.display='none'    
+})
+close12.addEventListener('click',function(){
+    modal11.style.display='none'    
+})
+close13.addEventListener('click',function(){
+    modal12.style.display='none'    
+ })
+close14.addEventListener('click',function(){
+    modal13.style.display='none'    
+})
+close15.addEventListener('click',function(){
+    modal14.style.display='none'    
+ })
+close16.addEventListener('click',function(){
+    modal15.style.display='none'    
+})
